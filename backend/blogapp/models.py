36a7820f3f9b5983c06edcc2e1b620a28b2c43b7
JoyceMideva <mideva061@gmail.com>
2023-08-18 17:14:44 +0200

    installed all apps

--- backend/blogapp/models.py
+++ backend/blogapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Blog(models.Model):
+    blog_title=models.CharField(max_length=150)
+    blog_post=models.TextField()
+    blog_author=models.CharField(max_length=30)
+    date_published=models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.blog_title     
--- backend/blogapp/views.py
+++ backend/blogapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Blog
+from .serializers import BlogSerializer
+from rest_framework.views import APIView 
+from rest_framework.response import Response
 # Create your views here.
+class BlogView(APIView):
+    def get(self,request,format=None):
+        blogs=Blog.objects.all()
+        serializers=BlogSerializer(blogs, many=True)
+        return Response(serializers.data)
