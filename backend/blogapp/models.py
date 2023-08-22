2939396a16c88668f070fcd095035a4da555d1d2
JoyceMideva <mideva061@gmail.com>
2023-08-22 11:48:43 +0200

    worked on the bakend and finished it

--- backend/blogapp/models.py
+++ backend/blogapp/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+def upload_to(instance,filename):
+    return "blogs/{filename}".format(filename=filename)
+
 class Blog(models.Model):
     blog_title=models.CharField(max_length=150)
     blog_post=models.TextField()
-    blog_author=models.CharField(max_length=30)
+    blog_image=models.ImageField(("image"),upload_to=upload_to,default="blogs/default.png")
+    blog_author=models.CharField(max_length=30,blank=True)
     date_published=models.DateTimeField(default=timezone.now())
 
     def __str__(self):
--- backend/blogapp/serializers.py
+++ backend/blogapp/serializers.py
@@ -4,4 +4,4 @@ from .models import Blog
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model=Blog
-        fields=('blog_title','blog_post','blog_author','date_published',)
+        fields=( 'id','blog_title','blog_image','blog_post','blog_author','date_published',)
